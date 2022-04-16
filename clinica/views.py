450d2aea57acb64f19264544171d68b01cd35bef
Francisco Carlos <carlos.junior@intmed.com.br>
2022-04-16 13:20:14 -0300

    add time and count queries context manager

--- clinica/views.py
+++ clinica/views.py
@@ -1,10 +1,20 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
 
 from .models import Medico
 from .serializers import MedicoSerializer
+from .utils.queries import CountQueries
+from .utils.time import timed
 
 
 class MedicoAPIView(ListAPIView):
     serializer_class = MedicoSerializer
     # permission_classes = [IsAuthenticated]
     queryset = Medico.objects.all()
+
+    @timed
+    def list(self, request):
+        with CountQueries():
+            queryset = self.get_queryset()
+            serializer = MedicoSerializer(queryset, many=True)
+            return Response(serializer.data)
